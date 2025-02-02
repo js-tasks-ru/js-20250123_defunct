b1798821df8f0552b0be6d4f423f600b337472a2
osvetilka <defunct@inbox.ru>
2025-02-03 02:41:50 +0500

    задача 3.1

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,9 +4,10 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-  const arr = path.split('.');
+  const arr = path.split('.');  
   
   return function(obj) {
+    if (!obj.hasOwnProperty(arr[0])) return ;
     return arr.reduce((accum, item)=> 
       accum.hasOwnProperty(item) ? accum[item] : undefined, obj)  
     
