924b6bb5259410b253e87692325c651008d66d80
osvetilka <defunct@inbox.ru>
2025-02-10 20:57:18 +0500

    Задача 5.1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,59 @@
-export default class NotificationMessage {
+export default class NotificationMessage{
+  message ;
+  duration;
+  type; 
+  element;
+ 
+  constructor(
+    message = '',
+    {duration = 1000, 
+     type = 'success'} = {}      
+  ) 
+{
+  this.message = message;
+  this.duration = duration;
+  this.type = type;  
+  this.element = this.createElement(this.createTemplate());
+//  debugger;
+}
 
+createTemplate() {
+  return (`    
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">${this.type}</div>
+        <div class="notification-body">
+          ${this.message}
+        </div>
+      </div>`
+  )
+}
+
+createElement(template) {
+  const element = document.createElement('div');
+  element.style = `--value:${Math.floor(this.duration / 1000)}s`;
+ // element.style = "--value:20s";
+  element.className = `notification ${this.type}`;
+  element.innerHTML = template;
+  return element ;
+}
+
+show(target = document.body) {  
+  if (!target.querySelector('div.notification') ) {        
+    target.append(this.element);    
+  }  
+  setTimeout(()=> {
+    this.remove()  
+  }, this.duration
+  );
+}
+
+remove() {
+  this.element.remove();
+}
+
+destroy() {
+  this.remove();
+}
+  
 }
